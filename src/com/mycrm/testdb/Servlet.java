3847e65a58719a9c421990f4ef0269e5298b7f73
BartlomiejBak <bakbartlomiej88>
2020-07-12 16:30:01 +0200

    Initial commit

---
+++ src/com/mycrm/testdb/Servlet.java
@@ -0,0 +1,42 @@
+package com.mycrm.testdb;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String user = "hbstudent";
+        String pass = "hbstudent";
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
+        String driver = "com.mysql.cj.jdbc.Driver";
+
+        try {
+            PrintWriter out = response.getWriter();
+
+            out.println("Connecting to database: " + jdbcUrl);
+
+            Class.forName(driver);
+
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+
+            out.println("SUCCESS!!!");
+
+            myConn.close();
+
+        } catch (Exception exc) {
+            exc.printStackTrace();
+            throw new ServletException(exc);
+        }
+    }
+
+}
---
+++ web/WEB-INF/view/list-customers.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: bakba
+  Date: 10.07.2020
+  Time: 16:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>List customers</title>
+</head>
+<body>
+Customers list
+</body>
+</html>
